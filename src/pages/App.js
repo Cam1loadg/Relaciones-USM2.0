3289f6cf5204bfd56f5cbe257d2bfbaacded8e02
Matty <mattybruh17@gmail.com>
2024-11-15 00:35:42 -0300

    Adición de elementos a HomePage

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../layout_pages/App.css';
 import backgroundImage from '../images/background.png';
 import InfoBecas from '../components/InfoBecas';
@@ -10,6 +10,9 @@ import Agenda from '../components/Agenda';
 import Becas from '../components/Becas';
 
 function MainApp() {
+  const qSomos = "La Dirección de Relaciones Estudiantiles contribuye a la formación integral de la comunidad estudiantil de todos los Campus y Sedes de la USM. Provee apoyo psicosocial, asistencial y de fomento a iniciativas del estudiantado, con el objetivo de acompañarlos y ayudarlos durante su vida universitaria, especialmente atendiendo al legado testamentario de nuestro fundador.";
+  const qOfrecemos = "Aquí se puede encontrar toda la información al respecto de las necesidades del estudiante de la USM. Además, al iniciar sesión, se obtiene la capacidad de solicitar directamente asistencia dental y psicólogica y poder adquirir beneficios, todo con el fin de mantener las condiciones óptimas para los estudiantes fuera del ámbito académico.";
+
   const [section, setSection] = useState("inicio");
   const [submenuVisible, setSubmenuVisible] = useState(false);
   const [submenuVisible2, setSubmenuVisible2] = useState(false);
@@ -84,18 +87,26 @@ function MainApp() {
       {section === "becas" && <Becas user={user} onLogin={handleLogin}/>}
       {section === "planes" && <Planes />}
       {section === "inicio" && (
-        <div className="content">
-          <p className="question">¿Quieres conocer tus beneficios?</p>
-          <a href="#cuenta" className="benefits-button" onClick={() => handleNavigation("cuenta")}>Ingresa Aquí</a>
-        </div>
+        <>
+          <div className="content">
+            <p className="question">¿Quieres conocer tus beneficios?</p>
+            <a href="#cuenta" className="benefits-button" onClick={() => handleNavigation("cuenta")}>Ingresa Aquí</a>
+          </div>
+          <div className='info-container'>
+            <div className='detail-container'>
+              <p className="detail-title">¿Quiénes somos?</p>
+              <div className='detail-text'>{qSomos}</div>
+            </div>
+            <div className='detail-container'>
+              <p className="detail-title">¿Qué ofrecemos?</p>
+              <div className='detail-text'>{qOfrecemos}</div>
+            </div>
+          </div >
+        </>
       )}
       {section === "psicologo" && <div className="content"></div> && <Agenda activeSection={"Agendar hora psicólogo"} user={user} onLogin={handleLogin}/>}
       {section === "dentista" && <div className="content"></div> && <Agenda activeSection={"Agendar hora dentista"} user={user} onLogin={handleLogin}/>}
 
-      {/* <div className='info-container'>
-        <div>yometrei dahs</div>
-        <div>globlaofgesfsfsfsefnsive</div>
-      </div> */}
       <a href="#ayuda" className="support-button" onClick={() => handleNavigation("ayuda")}>Ayuda</a>
       {section === "ayuda" && <Ayuda />}
     </div>
